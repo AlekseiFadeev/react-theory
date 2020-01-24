e0ab0ef366bf94c19314cfc705827e9d69dec60f
Aleksei Fadeev <33324706+AlekseiFadeev@users.noreply.github.com>
2020-01-25 02:44:42 +0300

    'state_change'

--- src/App.js
+++ src/App.js
@@ -21,10 +21,7 @@ class App extends Component{
         pageTitle: 'React components'
     }
 
-    changeTitleHandler = () => {
-        const oldTitle = this.state.pageTitle
-        let newTitle = oldTitle + ' (changed)'
-
+    changeTitleHandler = (newTitle) => {
         this.setState({
             pageTitle: newTitle
         })
@@ -38,21 +35,23 @@ class App extends Component{
                 <h1>{this.state.pageTitle}</h1>
 
                 <button
-                    onClick={this.changeTitleHandler}
+                    onClick={this.changeTitleHandler.bind(this, 'Changed!')}
                 >Change title</button>
 
                 <Car
                     name={cars[0].name}
                     year={cars[0].year}
-                    onChangeTitle={this.changeTitleHandler}
+                    onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
                 />
                 <Car
                     name={cars[1].name}
                     year={cars[1].year}
+                    onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
                 />
                 <Car
                     name={cars[2].name}
                     year={cars[2].year}
+                    onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
                 />
             </div>
         );
