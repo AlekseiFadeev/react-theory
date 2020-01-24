5f4f081522521a21b8f264da12aab5942996ffaf
Aleksei Fadeev <33324706+AlekseiFadeev@users.noreply.github.com>
2020-01-25 02:55:59 +0300

    return all elements from array

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,33 +1,5 @@
 import React from "react";
 
-// function car() {
-//     return (
-//         <div>This is car component</div>
-//     )
-// }
-
-// const car = () => {
-//     return (
-//         <div>This is car component</div>
-//     )
-// }
-
-// const car = () => (
-//     <div>
-//         This is car component
-//         <strong> Test</strong>
-//     </div>
-// )
-//
-// export default car;
-//
-// export default () => (
-//     <div>
-//         <p>This is car component</p>
-//         <p>Number: <strong>{Math.round(Math.random()*100)}</strong></p>
-//     </div>
-// )
-
 export default props => (
     <div>
         <h3>Car name: {props.name}</h3>
